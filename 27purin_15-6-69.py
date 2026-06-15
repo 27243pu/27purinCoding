ecb401303566a4039d3f0e5153cde710d24cf8c5
27243pu <27243@bbw.ac.th>
2026-06-15 09:53:20 +0700

    Add files via upload
    เขียนโปรแกรมบอกว่าเลขคือคู่หรือคี่ ได้เรียนรู้เรื่องการเขียนโปรแกรมมากขึ้น

---
+++ 27purin_15-6-69.py
@@ -0,0 +1,9 @@
+print("Is this a program for odd or even numbers?")
+number = int(input("Enter your number: "))
+
+if number % 2 == 0:
+    print("\neven numbers")
+else:
+    print("\nodd number")
+print("by pu4/4")
+print("thank you")
